b2203767925f78c33641297c99d5c0b7c208addf
nguyenlong205 <longnguyenhoang.personal@gmail.com>
2025-12-29 13:00:36 +0700

    update

--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,27 @@ def load_configs(config_path: str):
     except Exception as e:
         print(f"Lỗi không xác định: {e}")
         return {}
+
+import json
+from networkx.readwrite import json_graph
+
+class KGReader:
+    def __init__(self):
+        pass
+
+    def save_to_json(self,graph, filename="knowledge_graph.json"):
+
+        data = json_graph.node_link_data(graph)
+        
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"✅ Đã lưu graph vào {filename}")
+    
+    def load_from_json(filename="knowledge_graph.json"):
+        with open(filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        
+        # Khôi phục lại thành NetworkX Graph
+        graph = json_graph.node_link_graph(data)
+        print(f"✅ Đã load graph: {graph.number_of_nodes()} nodes")
+        return graph
